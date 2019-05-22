34eab00b79bca15fa7aad1e7800e658d8e737fca
Rodrigo Pituba de Souza <rodrigopitubasouza@live.com>
2019-05-22 12:08:57 -0300

    mudanças no caminho das paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'movies', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', loadChildren: './home/home.module#HomePageModule' },
   { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
   { path: 'movies/:id', loadChildren: './pages/movies-details/movies-details.module#MoviesDetailsPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' }
